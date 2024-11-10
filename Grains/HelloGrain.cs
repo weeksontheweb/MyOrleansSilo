f9673a88a8a0a905a02549760efe0e5595c0e6fd
weeksontheweb <andy@weeksontheweb.co.uk>
2024-11-10 20:49:00 +0000

    First commit.

---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,28 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class HelloGrain : Grain, IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger) => _logger = logger;
+
+        ValueTask<string> IHello.SayHello(string greeting)
+        {
+            _logger.LogInformation(
+                "SayHello message received: greeting = '{Greeting}'", greeting);
+
+            return ValueTask.FromResult(
+                $"""
+            Client said: '{greeting}', so HelloGrain says: Hello!
+            """);
+        }
+    }
+}
